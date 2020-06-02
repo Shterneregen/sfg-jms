c3289047e75db2e52d6367acee8d284fb3df304b
shterneregen <shterneregen@gmail.com>
2020-06-02 23:48:36 +0300

    adding message sender

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgjms;
 
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
 import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,15 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgJmsApplication {
 
     public static void main(String[] args) throws Exception {
+        initJmsServer();
+        SpringApplication.run(SfgJmsApplication.class, args);
+    }
 
-        ConfigurationImpl config = new ConfigurationImpl().setPersistenceEnabled(false)
+    public static void initJmsServer() throws Exception {
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
                 .setJournalDirectory("target/data/journal")
                 .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0");
-        ActiveMQServer server = new ActiveMQServerImpl(config);
+                .addAcceptorConfiguration("invm", "vm://0"));
         server.start();
-
-        SpringApplication.run(SfgJmsApplication.class, args);
     }
 
 }
